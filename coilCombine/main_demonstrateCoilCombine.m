55b636eaa8b5c9724317dd8751622be611f533ae
Fadil Abbas Ali <48543599+faa5115@users.noreply.github.com>
2025-05-27 11:21:12 -0400

    Add files via upload

--- coilCombine/main_demonstrateCoilCombine.m
+++ coilCombine/main_demonstrateCoilCombine.m
@@ -11,13 +11,27 @@ load('noiseForHipSlice.mat')
 %%
 imRaw = zeros(size(raw));
 
-figure,
+calib =  raw(:, round(Ny/2) - 14 : round(Ny/2) + 13, :, :);
+calibPad = zeros(size(raw));
+calibPad(:, round(Ny/2) - 14 : round(Ny/2) + 13, :, :) = raw(:, round(Ny/2) - 14 : round(Ny/2) + 13, :, :);
+
+
+% figure,
+% for chiter = 1 : Nc
+%     imRaw(:, :, :, chiter) = ifftnc(raw(:, :, :, chiter));
+%     subplot(5, 7, chiter)
+%     imshow(flip(abs(imRaw(:, :, :, chiter)), 1), [])
+%     title(strcat('Channel #', 32, num2str(chiter)))
+% end
+
+imCalib = zeros(size(calib));
+imCalibPad = zeros(size(calibPad));
 for chiter = 1 : Nc
     imRaw(:, :, :, chiter) = ifftnc(raw(:, :, :, chiter));
-    subplot(5, 7, chiter)
-    imshow(flip(abs(imRaw(:, :, :, chiter)), 1), [])
-    title(strcat('Channel #', 32, num2str(chiter)))
+    imCalib      (:, :, :, chiter) = ifftnc(calib      (:, :, :, chiter));
+    imCalibPad   (:, :, :, chiter) = ifftnc(calibPad   (:, :, :, chiter));
 end
+
 
 
 
@@ -28,6 +42,13 @@ figure,
 imshow(flip(abs(imRawsos), 1), [])
 title('Sq. SOS. Recon.')
 %% Now Walsh Adaptive Coil Combine
+
+figure,
+for chiter = 1 : Nc
+    subplot(5, 7, chiter)
+    imshow(flip(abs(imRaw(:, :, :, chiter)), 1), [])
+    title(strcat('Channel #', 32, num2str(chiter)))
+end
 
 figure,
 for chiter = 1 : Nc
@@ -46,9 +67,45 @@ end
 
 patch = [5, 5, 1, size(imRaw, 4)]; 
 [imRawcc] = func_WalshMethod(imRaw, noise, patch) ;
+%% SENSE recon:
+senseMapsOption = 1; % I have two options in generating sensitivity maps.  
+                     % Option 1 simply makes low resolution images from  
+                     % the center k-space lines and divides each low
+                     % resolution image by their square root sum of squares.
+                     % Option 2 uses a method called E-SPIRiT which I will
+                     % discuss in a later section. 
+R = [1, 1];  % This is "acceleration factor" along ky and kz encoding which I will discuss in the parallel imaging section.  
+             % We did not accelerate the acceleration in this example, so
+             % the values for both is 1. 
 
+[imRawSense, senseMaps] = func_SENSE(imRaw, imCalib, R, noise, senseMapsOption);
+
+%%
 figure,
 subplot(1, 2, 1)
-imshow(flip(abs(imRawcc), 1), [])
+imshow(flip(abs(imRawSense), 1), [])
+title('SENSE Recon. Magnitude')
 subplot(1, 2, 2)
+imshow(flip(angle(imRawSense), 1), [-pi, pi])
+title('SENSE Recon. Phase')
+%% show sense maps
+figure,
+for chiter = 1 : Nc
+    subplot(5, 7, chiter)
+    imshow(abs(senseMaps(:, :, :, chiter)), [])
+end
+%%
+
+figure,
+subplot(2, 2, 1)
+imshow(flip(abs(imRawSense), 1), [])
+title('SENSE Recon. Magnitude')
+subplot(2, 2, 2)
+imshow(flip(angle(imRawSense), 1), [-pi, pi])
+title('SENSE Recon. Phase')
+subplot(2, 2, 3)
+imshow(flip(abs(imRawcc), 1), [])
+title('Stochastic Matched Filter Magnitude')
+subplot(2, 2, 4)
 imshow(flip(angle(imRawcc), 1), [-pi, pi])
+title('Stochastic Matched Filter Magnitude')
