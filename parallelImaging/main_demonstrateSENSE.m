d1c30b0b8f1b1686fe735902c26073eed3e9902a
Fadil Abbas Ali <48543599+faa5115@users.noreply.github.com>
2025-05-28 17:25:32 -0400

    Add files via upload

--- parallelImaging/main_demonstrateSENSE.m
+++ parallelImaging/main_demonstrateSENSE.m
@@ -3,15 +3,22 @@
 % This is just done for simple 2D Cartesian imaging.  therefore the
 % unaliasing is just along 1D, the phase-encoding direction. 
 %%
-load('brain_8ch.mat'); NcSets = 1; % number of channel sets. 
+% load('brain_8ch.mat'); NcSets = 1; % number of channel sets. 
 % load('brain_alias_8ch.mat'); NcSets = 2; % number of channel sets. (for
 % e-spirit ... see my "simplified E-SPIRit" code.  
 
+% load whitened raw data and whitened noise
+ % raw = load('whitened2DACR.mat');
+ % raw = raw.rawW;
+ % noise = load('whitenedNoise4ACRData.mat');
+ % noise = noise.noiseMatw;
+ load('brain_8ch.mat');
+
 [Nx, Ny, Nz, Nc] = size(raw);
 
-R = 2;
+R =2; % 4;
 rawUs = zeros(size(raw));
-rawUs(:, 1 : R : end, :, :) = raw(:, 1 : 2 : end, :, :);
+rawUs(:, 1 : R : end, :, :) = raw(:, 1 : R : end, :, :);
 
 calib =  raw(:, round(Ny/2) - 14 : round(Ny/2) + 13, :, :);
 calibPad = zeros(size(raw));
@@ -45,7 +52,7 @@ imCalibPad_sos = func_sqSOS(imCalibPad, []);
 mask_im   = imRaw_cc   > 0.06 * max(imRaw_cc  (:));
 mask_imus = imRawUs_cc > 0.06 * max(imRawUs_cc(:));
 
-%%
+%% Display the full sampled image, low resolution calibration image, and the fully sampled image
 figure,
 subplot(3, 3, 1)
 imshow(abs(imRaw_cc), [])
@@ -57,109 +64,110 @@ subplot(3, 3, 3)
 imshow(log(abs(raw(:, :, 1, 1))), [])
 title('original fully sampled k-space')
 
-subplot(3, 3, 4)
-imshow(abs(imRawUs_cc), [])
-title('Undersampled image')
-subplot(3, 3, 5)
-imshow(angle(imRawUs_cc), [-pi, pi])
-title('Undersampled phase')
-subplot(3, 3, 6)
-imshow(log(abs(rawUs(:, :, 1, 1))), [])
-title('Undersampled k-space')
 
-subplot(3, 3, 7)
+
+subplot(3, 3, 4)
 imshow(abs(imCalibPad_cc), [])
 title('Low resolution image')
-subplot(3, 3, 8)
+subplot(3, 3, 5)
 imshow(angle(imCalibPad_cc), [-pi, pi])
 title('Low resolution phase')
-subplot(3, 3, 9)
+subplot(3, 3, 6)
 imshow(log(abs(calibPad(:, :, 1, 1))), [])
 title('Low resolution k-space')
 
-%% Generate Sensitivity maps.
-% To unalias the signal, we must generate sensitivity maps. A common
-% approach is to divide the channel images by the sq. sos. image.  I implemented a
-% simple least-squares version of E-SPIRiT, which I call "Simplified
-% E-SPIRiT."  I use this to generate the sensitivity maps. I will discuss
-% this in a separate main file, along with all of the inputs that are discussed 
-% in this section.  but for now just treat it as a funciton
-% that gives sensitivity maps: 
-
-% -------------------------------------------------------------------------
-% Inputs for Simplified E-Spirit. I will explain them in a different main
-% file. 
-kSize = [7, 7, 1];
-Nkx = kSize(1); Nky =  kSize(2); Nkz = kSize(3); numKernelShapes = 1;
-kernelShape = ones(Nkx, Nky, Nkz, numKernelShapes);
-% for kernelShapeIter = 1 : numKernelShapes
-%     kernelShape(:, 2, :, kernelShapeIter) = 0;
-% %     kernelShape(:, 1, :, kernelShapeIter) = 0;
-% end
-kSolveIndices = zeros(3, numKernelShapes);
-kSolveIndices(:, 1) = [4, 4, 1];
-kernelShape(kSolveIndices(1), kSolveIndices(2), kSolveIndices(3), 1) = 0;
-[ kernels, kernels_ones, kernels_solve] = func_generate_kernels(kernelShape,kSolveIndices);
-
-[weightsKernels, targetColumnIndex, sourceColumnIndex, ...
-    weightsKernels_decompressed, weightsKernels_reformatted, ...
-    Ac_sourceKernels_harmonicsx_reformatted, ...
-          Ac_sourceKernels_harmonicsy_reformatted, ...
-          Ac_sourceKernels_harmonicsz_reformatted]  = ...
-    func_getWeights_II(raw, kernelShape,kSolveIndices);
-[eigValuesM, eigVectorsM,mask_eig, mask_im] = ...
-    func_simplified_ESPIRiT(raw, kernelShape, kSolveIndices);
+subplot(3, 3, 7)
+imshow(abs(imRawUs_cc), [])
+title('Undersampled image')
+subplot(3, 3, 8)
+imshow(angle(imRawUs_cc), [-pi, pi])
+title('Undersampled phase')
+subplot(3, 3, 9)
+imshow(log(abs(rawUs(:, :, 1, 1))), [])
+title('Undersampled k-space')
 
 
-% The eigenvectors corresponding to eigenvalue 1 is the sense map.  I will
-% explain this in my main file discussion simplified e-spirit. 
-eigValue = 1;
-if NcSets == 1
-    eigValue = 1;
-    % sensemap = mask_eig(:, :, 1, eigValue) .* eigVectorsM(:, :, 1, :, eigValue);
-    sensemap = eigVectorsM(:, :, 1, :, eigValue);
-elseif NcSets == 2
-    sensemap = zeros(Nx, Ny, Nz, Nc * NcSets);
-    sensemap(:, :, :, 1 : Nc     ) = eigVectorsM(:, :, 1, :, 1);
-    sensemap(:, :, :, Nc+1 : 2*Nc) = eigVectorsM(:, :, 1, :, 2);
-end
 
-%%  Show magnitude and phse of sensitivity maps: 
-figure,
-for chiter = 1 : Nc
-    subplot(2, 4, chiter)
-    imshow(abs(squeeze(sensemap(:, :, 1, chiter))) .* ...
-        squeeze(mask_eig(:, :, :, 1)), [])
-    title(strcat('Magnitude of sens. map for channel',32,num2str(chiter)), ...
-        'fontsize', 10,'fontweight', 'bold')
-end
 
-figure, 
-for chiter = 1 : Nc
-    subplot(2, 4, chiter)
-    imshow(angle(squeeze(sensemap(:, :, 1, chiter))) .* ...
-        squeeze(mask_eig(:, :, :, 1)), [-pi, pi])
-    title(strcat('Phase of sens. map for channel',32,num2str(chiter)), ...
-        'fontsize', 10, 'fontweight','bold')
-end
 
 %%
-senseRecon = SENSE_fa1D(imRawUs, sensemap, 2);
+% senseRecon = SENSE_fa1D(imRawUs, sensemap, 2);
+
+% imCalibInput = imRaw;
+imCalibInput = imCalib; % ImCalib is the image from the low resolution region of k-space.  In this example, 
+
+
+senseMapsOption = 1; % this option takes the imCalib images and divide them by their square root sum of squares.  if you make it =2, I use E-SPIRiT to generate the sense maps.  I will discuss E-SPIRiT later. 
+noise = []; % I do not have noise data for this example. 
+
+Rinput = [R , 1];
+% Rinput is a vector of length 2.  The first index is the acceleration factor along ky and the second dimension is the acceleration along kz.
+% this is a 2D image example, so kz is fully sampled.  In this snippet of code R is either 2 or 4.  
+
+tic
+[imRawSense, senseMaps] = func_SENSE(imRawUs, imCalibInput, Rinput, noise, senseMapsOption);
+toc
+
+%%
+figure,
+
+
+subplot(3, 2, 1)
+imshow(abs(imRaw_cc), [])
+title('Recon Magnitude of fully sampled image ')
+
+subplot(3, 2, 2)
+imshow(angle(imRaw_cc), [-pi, pi])
+title('Recon Phase of fully sampled image')
+
+subplot(3, 2, 3)
+imshow(abs(imRawUs_cc), [])
+title(strcat(num2str(R),'x',32,'reduction recon magnitude'))
+
+subplot(3, 2, 4)
+imshow(angle(imRawUs_cc), [-pi, pi])
+title(strcat(num2str(R),'x',32,'reduction recon phase'))
+
+subplot(3, 2, 5)
+imshow(abs(imRawSense), [])
+title(strcat(num2str(R),'x',32,'acceleration sense recon magnitude'))
+
+subplot(3, 2, 6)
+imshow(angle(imRawSense), [-pi, pi])
+title(strcat(num2str(R),'x',32,'acceleration sense recon phase'))
+%%
+
+imSenseChannels = zeros(Nx, Ny, Nz, Nc);
+
+for chiter = 1 : Nc
+    senseMapiter = senseMaps(:, :, :, chiter);
+    imSenseChannels(:, :, :, chiter) = ...
+        senseMapiter .* imRawSense;
+end
+
+%% show imSenseChannels
 
 figure,
-subplot(2, 2, 1)
-imshow(abs(senseRecon), [])
-title('sense recon magnitude')
+for chiter = 1 : Nc
+    subplot(2, 4, chiter)
+    imshow(abs(imSenseChannels(:, :, :, chiter)), [])
+end
 
-subplot(2, 2, 2)
-imshow(angle(senseRecon), [-pi, pi])
-title('sense recon phase')
+figure,
+for chiter = 1 : Nc
+    subplot(2, 4, chiter)
+    imshow(angle(imSenseChannels(:, :, :, chiter)), [-pi, pi])
+end
+%% show original channels
 
-subplot(2, 2, 3)
-imshow(abs(imRaw_cc), [])
-title('magnitude of Walsh recon of fully sampled image ')
+figure,
+for chiter = 1 : Nc
+    subplot(2, 4, chiter)
+    imshow(abs(imRaw(:, :, :, chiter)), [])
+end
 
-subplot(2, 2, 4)
-imshow(angle(imRaw_cc), [-pi, pi])
-title('phase of Walsh recon of fully sampled image')
-
+figure,
+for chiter = 1 : Nc
+    subplot(2, 4, chiter)
+    imshow(angle(imRaw(:, :, :, chiter)), [-pi, pi])
+end
